ce2f0a0d046308af04271a41e7a513e1f66ad1e1
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-06-08 14:26:42 -0400

    added C# routes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todo-items", () => {});
+app.MapGet("/todo-items/{id}", (string id) => {
+    var todoItem = new {
+        Id = Guid.NewGuid().ToString(),
+        Title = "My Complete To-Do in C#",
+        IsCompleted = false
+    };
+    return Results.Ok(todoItem);
+});
+
+app.MapPost("/todo-items", () => {});
+app.MapPut("/todo-items/{id}", (string id) => {});
+app.MapDelete("/todo-items/{id}", (string id) => {});
 
 app.Run();
