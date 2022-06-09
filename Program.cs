4db2533bc8838be1f9def600b4b77654348732b1
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-06-08 21:57:57 -0400

    made api point with C#

--- Program.cs
+++ Program.cs
@@ -1,18 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Services;
+using Settings;
+using Models;
 
-app.MapGet("/todo-items", () => {});
-app.MapGet("/todo-items/{id}", (string id) => {
-    var todoItem = new {
-        Id = Guid.NewGuid().ToString(),
-        Title = "My Complete To-Do in C#",
-        IsCompleted = false
-    };
-    return Results.Ok(todoItem);
-});
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.Configure<TodoDbSettings>(builder.Configuration.GetSection("TodoDB"));
+        builder.Services.AddSingleton<ITodoItemService, TodoItemService>();
+        
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-app.MapPost("/todo-items", () => {});
-app.MapPut("/todo-items/{id}", (string id) => {});
-app.MapDelete("/todo-items/{id}", (string id) => {});
+        var app = builder.Build();
 
-app.Run();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+
+        app.MapGet("/todo-items", async (ITodoItemService todoItemService) =>
+        {
+            var todos = await todoItemService.GetAllTodoItems();
+            return Results.Ok(todos);
+        });
+
+        app.MapGet("/todo-items/{id}", async (string id, ITodoItemService todoItemService) =>
+        {
+
+            var todoItem = await todoItemService.GetTodoItemById(id);
+
+            return Results.Ok(todoItem);
+        });
+
+        app.MapPost("/todo-items", async (TodoItem newVoodoo, ITodoItemService service) =>
+        {
+            TodoItem createdTodoItem = await service.CreateTodoItem(newVoodoo);
+
+            return Results.Created($"todo-items/{createdTodoItem}", createdTodoItem);
+        });
+
+        app.MapPut("/todo-items/{id}", (string id) => { });
+        app.MapDelete("/todo-items/{id}", (string id) => { });
+
+        app.Run();
+    }
+}
